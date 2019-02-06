57e381979eda6ea383af304b8ec38efed0d37133
David Haugen <haugen.dj@gmail.com>
2019-02-06 10:43:01 -0600

    add first drill

---
+++ src/state-drills/HelloWorld.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class HelloWorld extends Component {
+  state = {
+    who: 'world'
+  };
+
+  handleWorldButtonClick = () => {
+    this.setState({...this.state, who: 'world'})
+  };
+
+  handleFriendButtonClick = () => {
+    this.setState({...this.state, who: 'friend'})
+  }
+
+  handleReactButtonClick = () => {
+    this.setState({...this.state, who: 'react'})
+  }
+
+  render() {
+    return (
+      <div className="HelloWorld">
+        <p>Hello {this.state.who}</p>
+        <button onClick = {this.handleWorldButtonClick} >World</button>
+        <button onClick = {this.handleFriendButtonClick}>Friend</button>
+        <button onClick = {this.handleReactButtonClick}>React</button>
+      </div>
+    );
+  }
+}
+
+export default HelloWorld;
