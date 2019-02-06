c94ffcd50112ff21135338b0a09fc794ea26c55e
owencyr <cyroc@dukes.jmu.edu>
2019-02-06 15:22:47 -0500

    roulette component implemented

--- src/state-drills/RouletteGun.js
+++ src/state-drills/RouletteGun.js
@@ -9,26 +9,27 @@ class RouletteGun extends Component {
   static defaultProps = {
     bulletInChamber : 8
   }
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+
 
   handleTrigger = () =>  {
     console.log('triggering!')
     this.setState({spinningTheChamber: true})
-    console.log(this.state);
-    this.timeout = setTimeout(this.checkChamber(), 2000)
+    // setTimeout(()=>{console.log(this.state)}, 100)
 
-  }
-
-  checkChamber = () => {
-    const bullet = Math.ceil(Math.random()*8);
-    this.setState({...this.state, chamber: bullet});
-    this.setState({...this.state, spinningTheChamber: false});
-    clearTimeout(this.timeout);
+    this.timeout = setTimeout(()=>{
+      const bullet = Math.ceil(Math.random()*8);
+      this.setState({chamber: bullet, spinningTheChamber:false});
+      // clearTimeout(this.timeout);
+    }, 1000)
   }
 
   render() {
     return (
     <div className="results">
-      <p>{(this.spinningTheChamber)? 'spinning the chamber and pulling the trigger! ...'
+      <p>{(this.state.spinningTheChamber)? 'spinning the chamber and pulling the trigger! ...'
         : (this.state.chamber === this.props.bulletInChamber)? 'BANG!!!!':"You're safe" }</p>
       <button onClick={this.handleTrigger}>Pull the Trigger!</button>
     </div>
